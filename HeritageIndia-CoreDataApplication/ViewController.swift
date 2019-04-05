d8dc09933717e2ddee19b65dbe85d6e8460f5bbb
Rohit <tyson@rohits-mac.local>
2019-04-05 15:37:08 -0700

    Initial Commit

---
+++ HeritageIndia-CoreDataApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeritageIndia-CoreDataApplication
+//
+//  Created by Rohit on 1/16/1398 AP.
+//  Copyright © 1398 Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
